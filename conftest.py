c846488c2c78ff56014398fe4fd119f430015258
Chris Chambers <magma.chambers@gmail.com>
2017-09-17 23:09:39 +0100

    Remove extraneous line from conftest

--- conftest.py
+++ conftest.py
@@ -8,4 +8,3 @@ def response():
     request = HTTPRequest("http://example.com")
     headers = HTTPHeaders({"content-type": "text/html"})
     return HTTPResponse(request, 200, headers)
-    return HTTPResponse(request, 200, {"content-type": "text/html\r\ncharset=ISO-8859-1"})
