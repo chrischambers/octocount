3353084bda731d83dadf2c0624bf2e446e2b02f7
Chris Chambers <magma.chambers@gmail.com>
2017-09-17 17:58:38 +0100

    Display vanilla bootstrap template

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+from os.path import abspath, dirname, join
 from tornado import web, wsgi
 from wsgiref.simple_server import make_server
+
+project_directory = abspath(dirname(__file__))
 
 
 class Index(web.RequestHandler):
     def get(self):
-        self.write("Hello World!")
+        self.render("index.html")
 
+
+settings = {
+    "template_path": join(project_directory, "templates"),
+    "static_path": join(project_directory, "static"),
+    # "debug": True,
+}
 
 app = web.Application([
     (r'/', Index),
-])
+], **settings)
+
 application = wsgi.WSGIAdapter(app)
 
 if __name__ == '__main__':
